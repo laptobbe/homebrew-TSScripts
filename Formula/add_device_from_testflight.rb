f1aa2887654f7d0f7701bd57010ad8f297a37ec3
Tobias Sundstrand <tobias.sundstrand@gmail.com>
2014-10-18 19:46:38 +0200

    First version of add device from test flight formula

---
+++ Formula/add_device_from_testflight.rb
@@ -0,0 +1,14 @@
+require "formula"
+
+class AddDeviceFromTestflight < Formula
+	homepage "https://github.com/laptobbe/TSScripts"
+	url "https://github.com/laptobbe/TSScripts.git"
+	version '0.0.1'
+
+	skip_clean 'bin'
+
+	def install
+		bin.install 'add_device_from_testflight'
+		(bin+'add_device_from_testflight').chmod 0755
+	end
+end
